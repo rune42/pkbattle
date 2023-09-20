eb706eb3c42a43de1bdd1e3da547bec658a80878
rune42 <mgonzalez.ecija@gmail.com>
2023-09-20 13:44:04 +0200

    FEAT: se añade estructura gráfica del menú

--- main.py
+++ main.py
@@ -4,30 +4,25 @@ from mons import *
 
 damage = random(0.8, 1) * (4 * (power * (attack / defense))/50)
 
-charmaner = Pykemon('Charmander', 4, 20, 20, 2, 4, 4, 6, Move()[''])
+charmander = Pykemon('Charmander', 4, 20, 20, 2, 4, 4, 6, Move()[''])
 
-if "__name__" == "__main__":
-    while(victory := False):
-        board = Board()
-        player = Player(board)
-        computer = Computer(board)
+print("Bienvenido a Pykémon! ¿Te has preparado para el combate?")
+print("Tu rival saca a Squirtle!")
+print("¡Adelante, Charmander!")
+while battle := True:
+    print("----------------------------------------------------------------")
+    print("¿Qué debería hacer Charmander?")
+    print("----------------------------------------------------------------")
+    cmd = input("------------LUCHAR--------------||--------------DATOS-----------")
+    if cmd.casefold() == "luchar" :
+        mv = input('''---------ARAÑAZO-----------||------------(vacío)---------\n
+        ... ----------------------------------------------------------------
+        ... ------------(vacío)-------------||------------(vacío)---------''')
+        pass
+    elif cmd.casefold() == "datos":
+        pass
+    else:
+        throw ValueError("El comando ingresado no es válido.")
 
-        while not victory:
-            print(board)
-            player_move = player.get_move()
-            computer_move = computer.get_move()
+    
 
-            if player_move == computer_move:
-                print("Tie!")
-            elif player_move == "X":
-                board.mark_board(player_move)
-            elif player_move == "O":
-                board.mark_board(computer_move)
-            else:
-                print("Invalid move!")
-
-            if board.is_board_full():
-                victory = True
-
-        print(board)
-        print(f"Winner: {board.get_winner()}")
