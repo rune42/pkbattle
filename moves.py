83c5280884162086423d9c93e8a287d7a7438c5b
rune42 <mgonzalez.ecija@gmail.com>
2023-10-09 13:06:23 +0200

    FEAT: finished moves class

--- moves.py
+++ moves.py
@@ -8,3 +8,13 @@ class Move:
     self.type = type
     self._power = power
     self._critChance = 0.075
+
+  def attack(self, attacker, defender):
+    if isinstance(attacker, Pykemon) and isinstance(defender, Pykemon):
+      if self.currentPP > 0:
+        damage = (random.uniform(0.8, 1) * (4 * self.power * (attacker.baseAtk / defender.baseDef)) / 50)
+        self.currentPP -= 1
+        return damage
+      else:
+        print("¡No quedan PP para usar este ataque!")
+        return None
