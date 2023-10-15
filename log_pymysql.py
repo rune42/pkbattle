c13c4c541d9f9300f21b0a0ab3beb818c771a8ba
Pau Pons <98814765+P2101@users.noreply.github.com>
2023-10-15 18:50:01 +0200

    Update log_pymysql.py

--- log_pymysql.py
+++ log_pymysql.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import pymysql
 from datetime import datetime
+from main import Pykemon, Battle
 
 # Parámetros de conexión
 host = 'localhost'
@@ -25,30 +26,22 @@ table_query = """
 CREATE TABLE IF NOT EXISTS data (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    victory BOOLEAN,
-    date DATE NOT NULL,
-  
+    date DATE NOT NULL
+
 );
 """
 
 cursor.execute(table_query)
 
-def generate_data():
-    # name = # DEBEMOS SACAR DEL MAIN
-    # victory = # DEBEMOS SACAR DEL MAIN
-    fecha = datetime.now()
-    
-    return fecha #, name, victory
+#OBTENER EL NOMBRE DEL GANADOR (NO VA)
+winner_name = Battle.battle()
+print("El ganador es:", winner_name)
 
-def insert_random_data():
-    data = generate_data()
-    insert_query = """
-    INSERT INTO data (fecha, num_estacion, precipitacion, temperatura, humedad)
-    VALUES (%s, %s, %s, %s, %s)
-    """
-    cursor.execute(insert_query, data)
+# Insertar los datos en la base de datos
+insert_query = "INSERT INTO data (name, date) VALUES (%s, %s)"
+cursor.execute(insert_query, (winner_name, datetime.now()))
 
+# Commit y cerrar la conexión
 connection.commit()
-insert_random_data()
 cursor.close()
 connection.close()
