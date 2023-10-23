f5f27db684eed598bd2b855d984ead5234eb83fd
Pau Pons <98814765+P2101@users.noreply.github.com>
2023-10-23 18:29:44 +0200

    Update main.py
    Main damage correcto

--- main.py
+++ main.py
@@ -17,27 +17,33 @@ class Pykemon(ABC):
     def descripcion_juego(self, name):
         pass
 
-    def harm(self, damage):
-        self.currentHP -= damage
-        if self.currentHP < 0:
-            self.currentHP = 0
-        return round(self.currentHP)
+    def harm(self, defender):
+        damage = self.attack()
+        defender.currentHP -= damage
+        if defender.currentHP < 0:
+            defender.currentHP = 0
+        return round(damage)
 
 class Electric(Pykemon):
     def __init__(self, name, dexNum, baseHP, baseAtk, baseDef, baseSpd):
         super().__init__(name, dexNum, baseHP, baseAtk, baseDef, baseSpd)
-        self.attack = lambda: round(random.uniform(0.8, 1) * (4 * (self.baseAtk * (self.baseAtk / self.baseDef)) / 50))
 
     def descripcion_juego(self, name):
         return f'Soy {name}, un Pykemon eléctrico!'
+    
+    def attack(self):
+        return round(random.uniform(0.6, 1) * (4 * (self.baseAtk * (self.baseAtk / self.baseDef)) / 50))
+
 
 class Fire(Pykemon):
     def __init__(self, name, dexNum, baseHP, baseAtk, baseDef, baseSpd):
         super().__init__(name, dexNum, baseHP, baseAtk, baseDef, baseSpd)
-        self.attack = lambda: round(random.uniform(0.8, 1) * (4 * (self.baseAtk * (self.baseAtk / self.baseDef)) / 50))
 
     def descripcion_juego(self, name):
         return f'Soy {name}, un Pykemon de fuego!'
+    
+    def attack(self):
+        return round(random.uniform(0.6, 1) * (4 * (self.baseAtk * (self.baseAtk / self.baseDef)) / 50))
 
 class Battle:
     def __init__(self, pykemons):
@@ -50,22 +56,17 @@ class Battle:
         while all(pykemon.currentHP > 0 for pykemon in self.pykemons):
             for i in range(len(self.pykemons)):
                 attacker = self.pykemons[i]
-                defender = self.pykemons[(i+1) % len(self.pykemons)]
+                defender = self.pykemons[(i + 1) % len(self.pykemons)]
 
-
-                damage = attacker.attack()
-                print(f'{attacker.name} hace un ataque de {damage}')
-                defender.harm(damage)
+                damage = attacker.harm(defender)
+                print(f'{attacker.name} hace un ataque de {damage} a {defender.name}')
 
                 if defender.currentHP <= 0:
                     print(f'{defender.name} ha sido derrotado. {attacker.name} ha ganado!')
-
-                    
                     return logs.get_name([attacker.name, defender.name])
 
                 print(f'La vida de {attacker.name} es {attacker.currentHP} y del {defender.name} es {defender.currentHP}')
                 time.sleep(2)
-
 
 pykemons = [Electric('Pikachu', 1, 18, 18, 5, 4), Fire('Charmander', 4, 18, 18, 5, 4)]
 battle = Battle(pykemons)
